eeb5dcc55c25bdcc20863fcd75c01e915a8f12a9
ReitoSerizawa <s.reitiger@gmail.com>
2022-03-24 21:49:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const removeMain = document.getElementById("main")
+removeMain.remove()
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = "YOUR-NAME is the champion"
